4022117f44b3657b57aef494edf3c1f7c0f85a27
huwenwen <wenwen.hu@ele.me>
2016-02-17 20:33:30 +0800

    merger

---
+++ trunk/operation_dao/src/main/java/com/eleme/dao/security/IUserSecurityDao.java
@@ -0,0 +1,30 @@
+package com.eleme.dao.security;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 权限控制dao接口。
+ * 
+ * @author penglau
+ *
+ */
+public interface IUserSecurityDao {
+
+  /**
+   * 查询所有权限集合
+   * 
+   * @return 权限封装集合
+   */
+  List<Map<String, Object>> findAllAuthorities();
+
+  /**
+   * 根据用户名查询用户信息
+   * 
+   * @param userId 用户id
+   * @param isAdmin 是否为管理员
+   * @return 用户权限信息
+   */
+  List<Map<String, Object>> findUsersSecurityByUserId(long userId, boolean iAdmin);
+
+}
